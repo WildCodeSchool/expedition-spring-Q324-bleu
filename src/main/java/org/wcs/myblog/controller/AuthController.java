eb5150e26ecccbb5f946451f602e945704dd60f3
AminaChakirDev <amina.aitm@gmail.com>
2025-04-06 20:14:55 +0200

    quete 13 - connexion

--- src/main/java/org/wcs/myblog/controller/AuthController.java
+++ src/main/java/org/wcs/myblog/controller/AuthController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.wcs.myblog.dto.UserLoginDTO;
 import org.wcs.myblog.dto.UserRegistrationDTO;
 import org.wcs.myblog.model.User;
+import org.wcs.myblog.security.AuthenticationService;
 import org.wcs.myblog.service.UserService;
 
 import java.util.Set;
@@ -16,9 +18,11 @@ import java.util.Set;
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthenticationService authenticationService) {
         this.userService = userService;
+        this.authenticationService = authenticationService;
     }
 
     @PostMapping("/register")
@@ -29,5 +33,14 @@ public class AuthController {
                 Set.of("ROLE_USER") // Par défaut, chaque utilisateur aura le rôle "USER"
         );
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticate(@RequestBody UserLoginDTO userLoginDTO) {
+        String token = authenticationService.authenticate(
+                userLoginDTO.getEmail(),
+                userLoginDTO.getPassword()
+        );
+        return ResponseEntity.ok(token);
     }
 }
